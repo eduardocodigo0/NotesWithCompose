0bd2647b3f472c4cb802dc3256386dabbf601589
eduardocodigo0 <eduardocodigo0@gmail.com>
2023-09-23 12:12:55 -0300

    Cards, search bar and fab

--- app/src/main/java/com/eduardocodigo0/noteswithcompose/MainActivity.kt
+++ app/src/main/java/com/eduardocodigo0/noteswithcompose/MainActivity.kt
@@ -6,41 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.eduardocodigo0.noteswithcompose.ui.theme.NotesWithComposeTheme
+import com.eduardocodigo0.noteswithcompose.ui.theme.AppTheme
+import com.eduardocodigo0.noteswithcompose.ui.view.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NotesWithComposeTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotesWithComposeTheme {
-        Greeting("Android")
-    }
-}
